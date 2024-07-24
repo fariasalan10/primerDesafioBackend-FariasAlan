d05f4bd99b8804f749e232d8507deac1e0c65b09
Farias Alan <alangft10@gmail.com>
2024-07-23 21:14:47 -0300

    added: completando login

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -22,3 +22,35 @@ loginForm.addEventListener("submit", (event) => {
     }
   });
 });
+
+const loginBtn = document.getElementById("login");
+const registerBtn = document.getElementById("register");
+
+const loginDiv = document.querySelector(".login-form");
+const registerDiv = document.querySelector(".register-form");
+
+loginBtn.addEventListener("click", () => {
+  loginBtn.style.backgroundColor = "blueviolet";
+  registerBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+
+  loginDiv.style.left = "50%";
+  registerDiv.style.left = "-50%";
+
+  loginDiv.style.opacity = "1";
+  registerDiv.style.opacity = "0";
+
+  document.querySelector(".col-1").style.borderRadius = "0 30% 20% 0";
+});
+
+registerBtn.addEventListener("click", () => {
+  registerBtn.style.backgroundColor = "blueviolet";
+  loginBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+
+  loginDiv.style.left = "150%";
+  registerDiv.style.left = "50%";
+
+  loginDiv.style.opacity = "0";
+  registerDiv.style.opacity = "1";
+
+  document.querySelector(".col-1").style.borderRadius = "0 20% 30% 0";
+});
